e8f6d932e2c158f15e0eb8f11c959183ef9ff8b9
ssuzyn <asj7090@gmail.com>
2023-08-11 13:39:05 +0900

    Refactor: AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig { //애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+        return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 }
