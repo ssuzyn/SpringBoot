c9a8cadd05dc2f8d5e0815db784c616a1e8a9886
ssuzyn <asj7090@gmail.com>
2023-08-11 14:27:59 +0900

    Refactor: 스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,28 +2,36 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { //애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     private static MemberRepository memberRepository() {
+
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
